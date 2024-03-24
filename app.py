98718212c3dd4b01aedae7724763ccf4802bb8fc
Martin Kim <127666042+The-Martin-Kim@users.noreply.github.com>
2024-03-25 00:18:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import pandas as pd
 import streamlit as st
 from urllib.parse import quote_plus
 
-df = pd.read_csv("isegye_data.csv")
-
 
 def search_songs(dataframe, search_query):
     search_query = search_query.lower()
@@ -22,6 +20,14 @@ def search_on_yt(keyword):
     return base_url + q
 
 
+def get_link(row):
+    query_parts = [row['Artist'], row['Title'], row.get('Artist2', ''), row.get('Artist3', '')]
+    key = " ".join(filter(None, query_parts))
+    return f"[지금 듣기]({search_on_yt(key)})"
+
+
+df = pd.read_csv("isegye_data.csv")
+
 st.title('ISEGYE Searchable Machine')
 
 query = st.text_input("키워드를 입력하세요: ")
@@ -30,14 +36,10 @@ if query:
     result = search_songs(df, query)
 
     if not result.empty:
-        result['YouTube'] = (result['Artist'].astype(str) + " " +
-                             result['Title'].astype(str) + " " +
-                             result['Artist2'].astype(str) + " " +
-                             result['Artist3'].astype(str)).apply(
-            lambda x: f"[지금 듣기]({search_on_yt(x)})")
-        result['Title'] = result['Title'].apply(lambda x: x[:20] + "..." if len(x) > 40 else x)
+        result['Title'] = result['Title'].apply(lambda x: x[:20] + "..." if len(x) > 20 else x)
         result['Artist2'] = result['Artist2'].fillna("")
         result['Artist3'] = result['Artist3'].fillna("")
+        result['YouTube'] = result.apply(get_link, axis=1)
 
         st.markdown(result[['Title', 'Artist', 'Artist2', 'Artist3', 'YouTube']].to_markdown(
             index=False), unsafe_allow_html=True)
